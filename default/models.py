2c63e31d94b1467a5582b24f0f3d5d5ee6b81d19
diddy <11330097@dcsh.tp.edu.tw>
2025-12-05 11:45:18 +0800

    1205

--- default/models.py
+++ default/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class polll(models.Model):
+    subject =models.CharField("投票主題", max_length=64)
+    desc =models.TextField("說明")
+    created =models .DateField("建立日期")
+
+
+class Option (models.Model):
+    title =models.CharField("選項文字", max_length=64)
+    votes = models.IntegerChoices("票數",default =0)
+    votes = models.IntegerChoices("投票主題編號")
